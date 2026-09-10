0bf36bf7b4b8859e770c7d0bc8112984595f306b
Hao-Ting Wang <haotingwang@protonmail.com>
2026-09-10 10:22:43 -0700

    fix output with missing values

--- src/preventad_benchmark/dataset/functional.py
+++ src/preventad_benchmark/dataset/functional.py
@@ -144,11 +144,7 @@ def extract_timeseries_from_nifti(source_denoised_dir, output_ts_dir, atlas="sch
         if seg_ts.shape[-1] ==len(atlas_labels):
             seg_ts = pd.DataFrame(seg_ts, columns=atlas_labels)
         else:
-            labels = [atlas_labels[i] for i in atlas_masker.region_index_]
+            labels = [atlas_labels[i] for i in atlas_masker.region_ids_]
             seg_ts = pd.DataFrame(seg_ts, columns=labels)
-            missings = set(atlas_labels) - set(labels)
-            for m in missings:
-                seg_ts[m] = np.nan
-            seg_ts = seg_ts.reindex(columns=atlas_labels)
-        # seg_ts = seg_ts.reindex(columns=atlas_labels)
+            seg_ts = seg_ts.reindex(columns=atlas_labels)  # this will fill missing with np.nan
         seg_ts.to_csv(ts_path, sep="\t", na_rep="n/a", index=False)
